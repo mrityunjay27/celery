7b34501c56557a3b5ea4b5a9c78a0e05e8c1ac80
mrityunjay27 <mrityunjay27062000@gmail.com>
2024-12-21 23:09:20 +0530

    Scheduling task with code.

--- myapp/tasks.py
+++ myapp/tasks.py
@@ -1,5 +1,10 @@
+import json
+from sys import intern
+
 from celery import shared_task
 from time import sleep
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
+
 
 @shared_task(name="sub_task")
 def sub(x, y):
@@ -16,3 +21,24 @@ def clear_redis_data(key):
     # Configured this from admin panel with the help of django celery beat.
     print("Redis data cleared: ", key)
     return key
+
+@shared_task()
+def clear_rabbitmq_data(key):
+    # Configured this from via code
+    print("RabbitMQ data cleared: ", key)
+    return key
+
+# Create a schedule to run every 20 seconds.
+schedule, created = IntervalSchedule.objects.get_or_create(
+    every=20,
+    period=IntervalSchedule.SECONDS
+)
+
+# Schedule the periodic task programmatically
+PeriodicTask.objects.get_or_create(
+    name='Clear RabbitMQ Periodic',
+    task='myapp.tasks.clear_rabbitmq_data',
+    interval=schedule,
+    args=json.dumps(['hello'])
+)
+
